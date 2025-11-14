9f0c2b4a0fc25fb89e884b7a3d808bf0baa59ad4
Tiago Campêlo <tiago.campelo@liferay.com>
2025-11-13 23:33:32 -0300

    update instruments

--- src/data/instruments.tsx
+++ src/data/instruments.tsx
@@ -1,23 +1,38 @@
 import fluteIcon from "../assets/instrument-icons/flute.png";
 import melodicaIcon from "../assets/instrument-icons/melodica.png";
 import ukuleleIcon from "../assets/instrument-icons/ukulele.png";
+import guitarIcon from "../assets/instrument-icons/guitar.png";
+import keyboardIcon from "../assets/instrument-icons/keyboard.png";
+import electrigGuitarIcon from "../assets/instrument-icons/electric-guitar.png";
 
 export type InstrumentDisplayData = {
-	image: string;
-	name: string;
+  image: string;
+  name: string;
 };
 
 export const instrumentsData: InstrumentDisplayData[] = [
-	{
-		image: fluteIcon,
-		name: "Flute",
-	},
-	{
-		image: melodicaIcon,
-		name: "Melodica",
-	},
-	{
-		image: ukuleleIcon,
-		name: "Ukulele",
-	},
+  {
+    image: fluteIcon,
+    name: "Flute",
+  },
+  {
+    image: melodicaIcon,
+    name: "Melodica",
+  },
+  {
+    image: ukuleleIcon,
+    name: "Ukulele",
+  },
+  {
+    image: guitarIcon,
+    name: "Guitar",
+  },
+  {
+    image: keyboardIcon,
+    name: "Keyboard",
+  },
+  {
+    image: electrigGuitarIcon,
+    name: "Electric Guitar",
+  },
 ];
